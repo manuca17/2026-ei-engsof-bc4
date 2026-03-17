91bfbec0ec11ed07a1e6f6e5cb15ab923fc57fbc
1Aurius <pedrogomesarias@gmail.com>
2026-03-17 16:49:50 +0000

    oops

---
+++ BlazorProject/Data/Models/TipoConsulta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorProject.Data.Models;
+
+public partial class TipoConsulta
+{
+    public int IdTipoConsulta { get; set; }
+
+    public string Descricao { get; set; } = null!;
+
+    public decimal? PrecoFixo { get; set; }
+
+    public decimal? PrecoHora { get; set; }
+
+    public int? IdUtilizador { get; set; }
+
+    public virtual ICollection<Consulta> Consulta { get; set; } = new List<Consulta>();
+
+    public virtual Utilizador? IdUtilizadorNavigation { get; set; }
+}
